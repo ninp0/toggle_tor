9dd840e212b7f778ce8043794556b8b5bfe7bd31
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-20 20:41:11 -0600

    Bugfix in PID retrieval

--- toggle_tor.sh
+++ toggle_tor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pid=$(ps -ef | grep '/usr/bin/tor' | grep -v grep | awk '{print $1}')
+pid=$(ps -ef | grep '/usr/bin/tor' | grep -v grep | awk '{print $2}')
 if [[ $pid != "" ]]; then
   echo "Killing Old PID: ${pid}"
   kill -9 $pid
