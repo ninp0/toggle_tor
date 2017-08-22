1e3faa39a9e40c48000745afec47c340e659b501
Jacob Hoopes <jake.hoopes@gmail.com>
2017-08-22 15:18:59 -0600

    Use sudo to kill tor if necessary

--- toggle_tor.sh
+++ toggle_tor.sh
@@ -4,6 +4,9 @@ pid=$(ps -ef | grep '/usr/bin/tor' | grep -v grep | awk '{print $2}')
 if [[ $pid != "" ]]; then
   echo "Killing Old PID: ${pid}"
   kill -9 $pid
+  if (( $? != 0 )); then
+    sudo kill -9 $pid
+  fi
   sleep 3
 fi
 
